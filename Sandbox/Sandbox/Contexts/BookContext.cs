e5a127d5964b988c8af3f55594751866265c5646
Donatas Vitkus <vitk.donatas@gmail.com>
2022-03-27 16:24:47 +0300

    Core done

---
+++ Sandbox/Sandbox/Contexts/BookContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sandbox.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sandbox.Contexts
+{
+    internal class BookContext : DbContext
+    {
+        public DbSet<Page> Pages { get; set; }
+        public DbSet<Book> Books { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlServer($"Server=localhost;Database=Sandbox;Trusted_Connection=True;");
+    }
+}
---
+++ Sandbox/Sandbox/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sandbox.Contexts;
+using System;
+using System.Linq;
+
+namespace Sandbox
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using var dbContext = new BookContext();
+            var book = dbContext.Books.Where(book => book.Id == new Guid("73F6C5E4-4E1B-4155-B8BA-48054E643A4B")).Include(x => x.Pages).First();
+            
+            //dbContext.Books cia musu visos knygos
+            //dbContext.Pages cia musu visi puslapiai
+            // naudoti grazinimui .ToList();
+
+            dbContext.Books.Remove(book);
+
+            dbContext.SaveChanges();
+        }
+    }
+}
